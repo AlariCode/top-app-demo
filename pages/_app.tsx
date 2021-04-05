d13a4ec45c0c2b2994fe352a819c627e63252f06
alaricode <antonlarichev@gmail.com>
2021-04-05 20:32:37 +0300

    Настройка eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
