da6cc02bb08742521f3647f900c961f04bdf0a0f
alaricode <antonlarichev@gmail.com>
2021-04-07 16:48:24 +0300

    Шрифты и стили

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,8 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 		<Head>
 			<title>MyTop - наш лучший топ</title>
 			<link rel="icon" href="/favicon.ico" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
 		</Head>
 		<Component {...pageProps} />
 	</>;
