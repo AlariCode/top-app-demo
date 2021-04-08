cfa71d8ee02bf576a0c1a48f98806095561e6a01
alaricode <antonlarichev@gmail.com>
2021-04-08 18:27:13 +0300

    Правила использования Hooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Button, Htag, P, Tag } from '../components';
 export default function Home(): JSX.Element {
 	return (
 		<>
-			<Htag tag='h1'>Текст</Htag>
+			<Htag tag='h1'>Заголовок</Htag>
 			<Button appearance='primary' arrow='right'>Кнопка</Button>
 			<Button appearance='ghost' arrow='down'>Кнопка</Button>
 			<P size='l'>Большой</P>
