7a4f6a22eddf39e861caceaeacdcf618db8fafac
alaricode <antonlarichev@gmail.com>
2021-04-14 19:33:22 +0300

    Упражнение - Вёртска footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,16 @@
 import { FooterProps } from './Footer.props';
-import styles from './Header.module.css';
+import styles from './Footer.module.css';
 import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
-		<div {...props}>
-			Footer
-		</div>
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>
+				OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+			</div>
+			<a href="#" target="_blank">Пользовательское соглашение</a>
+			<a href="#" target="_blank">Политика конфиденциальности</a>
+		</footer>
 	);
 };
