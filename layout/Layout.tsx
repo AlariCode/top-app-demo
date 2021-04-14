b314a3fb458b01366f68ce2a510c095fe73862ac
alaricode <antonlarichev@gmail.com>
2021-04-14 19:20:04 +0300

    Вёрстка Layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { LayoutProps } from './Layout.props';
-import styles from './P.module.css';
+import styles from './Layout.module.css';
 import cn from 'classnames';
 import { Header } from './Header/Header';
 import React, { FunctionComponent } from 'react';
@@ -8,16 +8,14 @@ import { Footer } from './Footer/Footer';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
-		<>
-			<Header />
-			<div>
-				<Sidebar />
-				<div>
-					{children}
-				</div>
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.body}>
+				{children}
 			</div>
-			<Footer />
-		</>
+			<Footer className={styles.footer} />
+		</div>
 	);
 };
 
