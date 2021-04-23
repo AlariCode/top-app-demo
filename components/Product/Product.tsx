4f27544f44bffef29c19cff809ed5e871f8cb79f
alaricode <antonlarichev@gmail.com>
2021-04-23 13:24:19 +0300

    Компонент Image

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -7,12 +7,18 @@ import { Tag } from '../Tag/Tag';
 import { Button } from '../Button/Button';
 import { declOfNum, priceRu } from '../../helpers/helpers';
 import { Divider } from '../Divider/Divider';
+import Image from 'next/image';
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	return (
 		<Card className={styles.product}>
 			<div className={styles.logo}>
-				<img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} />
+				<Image
+					src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+					alt={product.title}
+					width={70}
+					height={70}
+				/>
 			</div>
 			<div className={styles.title}>{product.title}</div>
 			<div className={styles.price}>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+	images: {
+		domains: ['courses-top.ru']
+	},
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/,
