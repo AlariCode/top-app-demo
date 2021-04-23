d61079768e24a09b537073d005f8e23b0755f813
alaricode <antonlarichev@gmail.com>
2021-04-23 16:30:16 +0300

    Компонент продукта - адаптив

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -54,7 +54,7 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 					<div>{product.disadvantages}</div>
 				</div>}
 			</div>
-			<Divider className={styles.hr} />
+			<Divider className={cn(styles.hr, styles.hr2)} />
 			<div className={styles.actions}>
 				<Button appearance='primary'>Узнать подробнее</Button>
 				<Button appearance='ghost' arrow={'right'} className={styles.reviewButton}>Читать отзывы</Button>
