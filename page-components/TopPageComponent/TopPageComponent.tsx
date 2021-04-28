3f65b97215aa25581822f5c1a9723c5e788f1b59
alaricode <antonlarichev@gmail.com>
2021-04-28 19:06:39 +0300

    Упражнение - Исправление бага useReducer

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -4,7 +4,7 @@ import styles from './TopPageComponent.module.css';
 import { HhData } from '../../components';
 import { TopLevelCategory } from '../../interfaces/page.interface';
 import { SortEnum } from '../../components/Sort/Sort.props';
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import { sortReducer } from './sort.reducer';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
@@ -13,6 +13,10 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 	const setSort = (sort: SortEnum) => {
 		dispathSort({ type: sort });
 	};
+
+	useEffect(() => {
+		dispathSort({ type: 'reset', initialState: products });
+	}, [products]);
 
 	return (
 		<div className={styles.wrapper}>
--- page-components/TopPageComponent/sort.reducer.ts
+++ page-components/TopPageComponent/sort.reducer.ts
@@ -1,7 +1,7 @@
 import { SortEnum } from '../../components/Sort/Sort.props';
 import { ProductModel } from '../../interfaces/product.interface';
 
-export type SortActions = { type: SortEnum } | { type: SortEnum.Rating };
+export type SortActions = { type: SortEnum } | { type: SortEnum.Rating } | { type: 'reset', initialState: ProductModel[] };
 
 export interface SortReducerState {
 	sort: SortEnum;
@@ -19,6 +19,11 @@ export const sortReducer = (state: SortReducerState, action: SortActions): SortR
 			return {
 				sort: SortEnum.Price,
 				products: state.products.sort((a, b) => a.price > b.price ? 1 : -1)
+			};
+		case 'reset':
+			return {
+				sort: SortEnum.Rating,
+				products: action.initialState
 			};
 		default:
 			throw new Error('Неверный тип сортировки');
