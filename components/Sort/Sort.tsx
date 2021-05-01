be889c07041588225d0b03f73daeec19d6118680
alaricode <antonlarichev@gmail.com>
2021-05-01 14:32:18 +0300

    Упражнение - доступность сортировки

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -6,22 +6,22 @@ import cn from 'classnames';
 export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
 	return (
 		<div className={cn(styles.sort, className)} {...props}>
-			<span
+			<button
 				onClick={() => setSort(SortEnum.Rating)}
 				className={cn({
 					[styles.active]: sort == SortEnum.Rating
 				})}
 			>
 				<SortIcon className={styles.sortIcon} />По рейтингу
-			</span>
-			<span
+			</button>
+			<button
 				onClick={() => setSort(SortEnum.Price)}
 				className={cn({
 					[styles.active]: sort == SortEnum.Price
 				})}
 			>
 				<SortIcon className={styles.sortIcon} />По цене
-			</span>
+			</button>
 		</div>
 	);
 };
