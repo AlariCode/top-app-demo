c575f7badf5b3ee823856ee602f65c3ec1c66b1e
alaricode <antonlarichev@gmail.com>
2021-05-06 19:11:56 +0300

    Добавление meta на страницу

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -9,13 +9,22 @@ import { ProductModel } from '../../interfaces/product.interface';
 import { firstLevelMenu } from '../../helpers/helpers';
 import { TopPageComponent } from '../../page-components';
 import { API } from '../../helpers/api';
+import Head from 'next/head';
 
 function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
-	return <TopPageComponent
-		firstCategory={firstCategory}
-		page={page}
-		products={products}
-	/>;
+	return <>
+		<Head>
+			<title>{page.metaTitle}</title>
+			<meta name="description" content={page.metaDescription} />
+			<meta property="og:title" content={page.metaTitle} />
+			<meta property="og:description" content={page.metaDescription} />
+			<meta property="og:type" content="article" />
+		</Head>
+		<TopPageComponent
+			firstCategory={firstCategory}
+			page={page}
+			products={products}
+		/></>;
 }
 
 export default withLayout(TopPage);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,15 @@ import '../styles/globals.css';
 import Head from 'next/head';
 import React from 'react';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
 	return <>
 		<Head>
 			<title>MyTop - наш лучший топ</title>
 			<link rel="icon" href="/favicon.ico" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
 			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+			<meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+			<meta property="og:locale" content="ru_RU" />
 		</Head>
 		<Component {...pageProps} />
 	</>;
