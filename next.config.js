31cde4d1100fa2ec2494258cd93fa979cfa4d0e4
Alaricode <antonlarichev@gmail.com>
2021-12-16 10:35:50 +0300

    Дополнительно - Обновление SVGR

--- next.config.js
+++ next.config.js
@@ -5,11 +5,19 @@ module.exports = {
 	webpack(config, options) {
 		config.module.rules.push({
 			loader: '@svgr/webpack',
+			issuer: /\.[jt]sx?$/,
 			options: {
 				prettier: false,
 				svgo: true,
 				svgoConfig: {
-					plugins: [{ removeViewBox: false }],
+					plugins: [{
+						name: 'preset-default',
+						params: {
+							override: {
+								removeViewBox: false
+							}
+						}
+					}],
 				},
 				titleProp: true,
 			},
