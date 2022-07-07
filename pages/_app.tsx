2f54f21b00566e8e23a19caec5d1433e1e14d0fe
Demian <dnetlyukh@gmail.com>
2022-07-07 13:42:29 +0300

    Save alternative changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
+import React from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// eslint-disable-next-line react/prop-types
+function MyApp({ Component, pageProps }): JSX.Element {
+	return ( <>
+		<Head>
+			<title>My top 123</title>
+			<link rel="icon" href="favicon.ico" />
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+		</Head>
+		<Component {...pageProps} />
+	</> );
 }
 
-export default MyApp
+export default MyApp;
